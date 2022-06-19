b23e70440b545e49276894c8b7425d83afd8cb03
Constantin <creck_er@mail.ru>
2022-06-19 22:27:53 +0200

    Secend

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let x = "verschide  kommision";
+
+let obj = {
+    name: "Alex",
+    age: 12,
+    bool: true
+};
+
+console.log(obj);
+console.log(obj['age']);
+console.log(obj.age);
